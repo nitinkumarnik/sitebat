f5d50be4ade76605ccd9ae78a4b4a862e8716a34
Nitin NiK <nitinkumardj@live.com>
2019-12-30 16:26:08 +0530

    static fixing

--- socialbat/settings.py
+++ socialbat/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'socialbat.urls'
 
